cd00b1d5a3f70749141dac93473d11a2cdc1be63
kyawthura-gg <ggaryal@gmail.com>
2020-12-23 22:09:14 +0630

    created category and sub category table

---
+++ database/migrations/2020_12_23_035513_create_sub_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->boolean('is_visible')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_categories');
+    }
+}
