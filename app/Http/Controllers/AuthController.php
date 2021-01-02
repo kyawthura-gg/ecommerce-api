85dff3f2b7a99a07852998b1a130797384f137bc
kyawthura-gg <ggaryal@gmail.com>
2021-01-02 21:26:45 +0630

    fixed error message

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -59,7 +59,7 @@ class AuthController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 400);
+            return response()->json(['message' => $validator->messages()->first()], 400);
         }
 
         $user = User::create(array_merge(
